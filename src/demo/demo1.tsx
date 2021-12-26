c493516311be8fb3a7da61ebb85804eef8517774
JadannWu <wuqiongdan@szsandstone.com>
2021-12-26 11:22:03 +0800

    feat: 初始化项目

---
+++ src/demo/demo1.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+  render() {
+    return <button>click</button>;
+  }
+}
+
+export default Button; // Don’t forget to use export default!
